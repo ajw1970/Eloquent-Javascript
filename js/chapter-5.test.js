78af41911633f809f8b6537ab4b719666ba050d1
John Welty <john@weltyautomation.com>
2022-05-06 14:35:13 -0500

    Move functions to outer scope and get nth working

--- js/chapter-5.test.js
+++ js/chapter-5.test.js
@@ -1,19 +1,35 @@
 const { describe, it, expect } = require('@jest/globals');
 
-describe('ArrayToList', () => {
-
-    function arrayToList(arr) {
-        let list = {};
-        for (let i = arr.length -1; i >= 0; i--) {
-            list = {
-                value: arr[i],
-                rest: list.hasOwnProperty("value") ? list : null,
-            };
-            console.log(list.hasOwnProperty("value"));
-        }
-        return list;
+function arrayToList(arr) {
+    let list = {};
+    for (let i = arr.length - 1; i >= 0; i--) {
+        list = {
+            value: arr[i],
+            rest: list.hasOwnProperty("value") ? list : null,
+        };
+        console.log(list.hasOwnProperty("value"));
     }
+    return list;
+}
 
+function listToArray(list) {
+    let result = [];
+    while (list.rest !== null) {
+        result.push(list.value);
+        list = list.rest;
+    }
+    result.push(list.value);
+    return result;
+}
+
+function prepend(element, list) {
+    return {
+        value: element,
+        rest: list,
+    };
+}
+
+describe('ArrayToList', () => {
     it('builds up list structure', () => {
         let result = arrayToList([10,20]);
 
@@ -24,17 +40,6 @@ describe('ArrayToList', () => {
 })
 
 describe('listToArray', () => {
-
-    function listToArray(list) {
-        let result = [];
-        while (list.rest !== null) {
-            result.push(list.value);
-            list = list.rest;
-        }
-        result.push(list.value);
-        return result;
-    }
-
     it('produces array from a list', () => {
         let result = listToArray({ value: 10, rest: { value: 20, rest: null } });
 
@@ -45,18 +50,10 @@ describe('listToArray', () => {
 })
 
 describe('prepend', () => {
-
-    function prepend(element, list) {
-        return {
-            value: element,
-            rest: list,
-        };
-    }
-
     it('adds the element to the front of the input list', () => {
         let result = prepend(10, prepend(20, null));
 
-        let expected = { value: 10, rest: { value: 20, rest: null } };
+        let expected = arrayToList([10, 20]);
 
         expect(result).toStrictEqual(expected);
     })
@@ -65,21 +62,18 @@ describe('prepend', () => {
 describe('nth', () => {
 
     function nth(list, index) {
-
+        return listToArray(list)[index];
     }
 
-    it('returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element', () => {
-        let result = nth({
-            value: 10,
-            rest: {
-                value: 20,
-                rest: {
-                    value: 30,
-                    rest: null
-                }
-            }
-        }, 1);
+    it('returns the element at the given position in the list (with zero referring to the first element)', () => {
+        let result = nth(arrayToList([10, 20, 30]), 1);
 
         expect(result).toBe(20);
+    })
+
+    it('returns undefined when there is no such element', () => {
+        let result = nth(arrayToList([10, 20]), 99);
+
+        expect(result).toBeUndefined();
     })
 })
