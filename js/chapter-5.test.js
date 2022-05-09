46acd2f5035c0b3b37b6a056530681c71aedefe3
John Welty <john@weltyautomation.com>
2022-05-09 07:12:21 -0500

    Add test text

--- js/chapter-5.test.js
+++ js/chapter-5.test.js
@@ -1,7 +1,7 @@
 const { describe, it, expect } = require('@jest/globals');
 
 describe('flattenArrays', () => {
-    it('', () => {
+    it('Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.', () => {
         let arrays = [[1, 2, 3], [4, 5], [6]];
 
         expect(flattenArrays(arrays)).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6]));
