281020c7502e7aa73660f5cf4787b130f1cc43e3
John Welty <john@weltyautomation.com>
2022-05-09 07:12:36 -0500

    Add function for looping

--- js/chapter-5.test.js
+++ js/chapter-5.test.js
@@ -12,3 +12,27 @@ describe('flattenArrays', () => {
     }
 
 })
+
+describe('loop', () => {
+    it('Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning. When defining the function, you can use a regular loop to do the actual looping.', () => {
+        const logSpy = jest.spyOn(console, 'log');
+
+        //act
+        loop(3, n => n > 0, n => n - 1, console.log);
+
+        expect(logSpy).toHaveBeenNthCalledWith(1, 3);
+        expect(logSpy).toHaveBeenNthCalledWith(2, 2);
+        expect(logSpy).toHaveBeenNthCalledWith(3, 1);
+    })
+
+    /**
+    It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning.
+    */
+    function loop(value, testFn, updateFn, bodyFn) {
+        bodyFn(value);
+        if (testFn(value)) {
+            value = updateFn(value);
+            loop(value, testFn, updateFn, bodyFn);
+        }
+    }
+})
