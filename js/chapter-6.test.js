35a67e057ea1a11ca01e287e809ce6b94d878172
John Welty <john@weltyautomation.com>
2022-05-17 06:43:09 -0500

    Build out Vector assignment

--- js/chapter-6.test.js
+++ js/chapter-6.test.js
@@ -15,3 +15,45 @@ describe('prototypes', () => {
         expect(Object.getPrototypeOf([])).toStrictEqual(Array.prototype);
     })
 })
+
+describe('Vec', () => {
+
+    class Vec {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        plus(vector) {
+            return new Vec(this.x + vector.x, this.y + vector.y);
+        }
+
+        minus(vector) {
+            return new Vec(this.x - vector.x, this.y - vector.y);
+        }
+
+        get length() {
+            return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+        }
+    }
+
+    it('represents a vector in two-dimensional space. It takes x and y parameters (numbers), which it should save to properties of the same name.', () => {
+        const vector = new Vec(1, 2);
+        expect(vector.x).toStrictEqual(1);
+        expect(vector.y).toStrictEqual(2);
+    })
+
+    it('Give the Vec prototype two methods, plus and minus, that take another vector as a parameter and return a new vector that has the sum or difference of the two vectors’ (this and the parameter) x and y values.', () => {
+        const vSum = new Vec(1, 2).plus(new Vec(2, 3));
+        expect(vSum.x).toStrictEqual(3);
+        expect(vSum.y).toStrictEqual(5);
+
+        const vDiff = new Vec(1, 2).minus(new Vec(2, 3));
+        expect(vDiff.x).toStrictEqual(-1);
+        expect(vDiff.y).toStrictEqual(-1);
+    })
+
+    it('Add a getter property length to the prototype that computes the length of the vector—that is, the distance of the point (x, y) from the origin (0, 0).', () => {
+        expect(new Vec(3, 4).length).toStrictEqual(5);
+    })
+})
